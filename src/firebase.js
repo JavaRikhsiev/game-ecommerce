dcb6d51e0ce6a74bcf63ad7e732f45fb60ceb3e9
JavaRikhsiev <Javohir_99@inbox.ru>
2022-01-29 16:10:11 +0500

    firebase configuration added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLqelDE6ORJ1NcwmpvUjPhz_dfdDhC6JU",
+  authDomain: "game-commerce-89714.firebaseapp.com",
+  projectId: "game-commerce-89714",
+  storageBucket: "game-commerce-89714.appspot.com",
+  messagingSenderId: "733748842248",
+  appId: "1:733748842248:web:bbfc7000b35b5d62b966bd",
+  measurementId: "G-5MX2K2NPYT",
+};
